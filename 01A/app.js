0896299541bc9cb996364d245b7add63011bc66f
Yunus <38208@ma-web.nl>
2023-11-21 12:13:55 +0100

    opdracht2

--- 01A/app.js
+++ 01A/app.js
@@ -3,6 +3,20 @@ class App
     runApplication()
     {
         console.log("helloworld!");
+        let appNaam = "Angry Birds"
+        console.log("appNaam: " + appNaam);
+        let versienummer = "1.2"
+        console.log("verisenummer: " + versienummer);
+        let versiedatum = "16/11/2023 14:56:21"
+        console.log("versiedatum: " + versiedatum);
+        let autheur = "Yunus"
+        console.log("autheur: " + autheur);
+        let copyright = "Rovio"
+        console.log("copyright: " + copyright);
+        let distributeur = "Sega"
+        console.log("distributeur: " + distributeur)
+        let darkmode = true
+        console.log("darkmode: " + darkmode)
     }
 }
 
