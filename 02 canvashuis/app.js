e1ac773d411cd32fd277419db32f5527882661b1
Yunus <38208@ma-web.nl>
2023-11-23 12:05:28 +0100

    les2_opdracht2

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -2,9 +2,14 @@ class App
 {
     runApplication()
     {
+        
+        
         let canvas = document.getElementById("canvasId")
         let g = canvas.getContext("2d");
         console.log(canvas)
+        g.fillStyle = "blue";
+        g.fillRect(0,0,canvas.width,canvas.height);
+        g.fillStyle = "yellow";
         g.fillRect(0,0,10,10);
 
     }
