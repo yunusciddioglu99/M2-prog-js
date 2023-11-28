9d67d51ad96e0b49398cea55665798c3b2cd4d26
Yunus <38208@ma-web.nl>
2023-11-28 12:42:43 +0100

    les3_opdracht3

---
+++ 03 ifelse huisje/app.js
@@ -0,0 +1,87 @@
+class App
+{
+    runApplication()
+    {
+        
+        
+        let canvas = document.getElementById("canvasId")
+        let g = canvas.getContext("2d");
+        console.log(canvas)
+        g.beginPath();
+        g.fillStyle = "green"
+        g.moveTo(20,30);
+        g.lineTo(20,50);
+        g.lineTo(60,60);
+        g.lineTo(60,40);
+        g.closePath();
+        g.stroke();
+        g.fill();
+
+
+
+
+        g.beginPath();
+        g.fillStyle = "brown"
+        g.moveTo(30,10);
+        g.lineTo(20,30);
+        g.lineTo(60,40);
+        g.lineTo(70,20);
+        g.closePath();
+        g.stroke();
+        g.fill();
+
+
+        g.beginPath();
+        g.fillStyle = "brown"
+        g.moveTo(60,40);
+        g.lineTo(70,20);
+        g.lineTo(80,30);
+        g.closePath();
+        g.stroke();
+        g.fill();
+
+
+        g.beginPath();
+        g.fillStyle = "green"
+        g.moveTo(60,40);
+        g.lineTo(80,30);
+        g.lineTo(80,50);
+        g.lineTo(60,60);
+        g.closePath();
+        g.stroke();
+        g.fill();
+
+
+        g.beginPath();      
+        let random = Math.random();
+        console.log(random);
+
+        if(random < 0.3)
+      {
+        g.fillStyle = "#eeff00"
+      } else
+      {
+        g.fillStyle  = "#000000"
+      }
+
+        g.moveTo(35,35);
+        g.lineTo(45,38);
+        g.lineTo(45,48);
+        g.lineTo(35,45);
+        g.closePath();
+        g.stroke();
+        g.fill();
+
+
+
+
+
+
+
+
+        g.lineTo();
+    }
+}
+
+let app = new App();
+app.runApplication();
