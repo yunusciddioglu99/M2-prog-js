660db28a3b72cc10019c73a5f35042f04b1050f3
Yunus <38208@ma-web.nl>
2023-12-05 12:06:18 +0100

    les4_opdracht3

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -1,3 +1,60 @@
+function heeftEenResultaat()
+{
+    let resultaat =1;
+    return resultaat;
+}
+
+let hetResultaat = heeftEenResultaat();
+console.log(hetResultaat);
+console.log(heeftEenResultaat());
+
+
+let x = 9;
+let a = 3;
+let b = 4;
+let c = 89;
+
+let y = (a*(x*x) )+ (b*x) +c;
+console.log(y);
+
+
+function ax2bcWiskunde(x,a,b,c)
+{
+    let y = (a*(x*x) )+ (b*x) +c;
+    return y;
+}
+
+let y1 = ax2bcWiskunde(9,3,4,89);
+console.log(y1);
+
+let y2 = ax2bcWiskunde(3,6,5,45);
+console.log(y2);
+
+let y3 = ax2bcWiskunde(7,1,2,13);
+console.log(y3);
+
+
+
+let u = 9;
+let h = 8;
+let l = 15;
+let logY = Math.log(l) + h + Math.pow(u,2);
+console.log(logY);
+
+function BerekeningTest(u,h,l)
+{
+    let logY = Math.log(l) + h + Math.pow(u,2);
+    return logY;
+}
+
+
+let logY1 = BerekeningTest(14,32,66)
+console.log(logY1);
+
+let logY2 = BerekeningTest(55,99,73)
+console.log(logY2);
+
+
 function argumentsAreHandy(shoutout)
 {
     console.log("do you want to give a shoutout")
