7e9ffdb032c5e18610400be4618bfa09bac47b3e
Yunus <38208@ma-web.nl>
2023-12-05 12:11:22 +0100

    les4_opdracht4

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -80,9 +80,25 @@ class App
     runApplication()
     {
         console.log("hello world!");
-        superMooieGlobalFunction();
     }
+
+    newClassFunction()
+    {
+        console.log("hello world in de nieuwClassFunction");
+    }
+
+    anotherFunctionWithArguments(aArgument)
+    {
+        console.log("kom maar met je argument");
+        console.log("hier:");
+        console.log(aArgument);
+
+    }
+
+
 }
 
 let app = new App();
 app.runApplication();
+app.newClassFunction();
+app.anotherFunctionWithArguments("mijn argument is iets...");
