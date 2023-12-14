9cc2a1d091f6ba18e13b47cc8a4d6e9d35366724
Yunus <38208@ma-web.nl>
2023-12-14 11:57:15 +0100

    les5_opdracht2

--- 05 arrays/app.js
+++ 05 arrays/app.js
@@ -10,6 +10,35 @@ class App
             console.log(i+ ": " + element);
         }
 
+        array.push("bob dylan");
+        array.push("prince");
+
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+            console.log(i+ ": " + element);
+        }
+
+
+
+    let indexToRemove = array.indexOf("bob dylan");
+    array.splice(indexToRemove,1);
+
+    let indexToRemove1 = array.indexOf("prince");
+    array.splice(indexToRemove1,1);
+
+    array.push("Ado");
+    array.push("Lil Uzi Vert");
+
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        console.log(i+ ": " + element);
+    }
+
+
+
+
+
 
         let arraynummers = [9, 8, 7];
 
